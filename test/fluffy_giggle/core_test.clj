b36f8fce59973f734fe7f5dbf14d9b1f738d8293
vijaykiran <mail@vijaykiran.com>
2015-12-12 20:53:17 +0100

    Init

---
+++ test/fluffy_giggle/core_test.clj
@@ -0,0 +1,7 @@
+(ns fluffy-giggle.core-test
+  (:require [clojure.test :refer :all]
+            [fluffy-giggle.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
